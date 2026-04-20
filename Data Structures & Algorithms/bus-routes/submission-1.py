72e82d0a248e7e87e0c4032e0e5f5047241e05eb
Spyridon Giakoumatos <105256569+millerdefoe@users.noreply.github.com>
2026-04-20 18:03:50 +0800

    Add: bus-routes - submission-1

---
+++ Data Structures & Algorithms/bus-routes/submission-1.py
@@ -0,0 +1,32 @@
+class Solution:
+    def numBusesToDestination(self, routes: List[List[int]], source: int, target: int) -> int:
+        if source == target:
+            return 0
+
+        stop_to_routes = defaultdict(set)
+        for i in range(len(routes)):
+            for j in routes[i]:
+                stop_to_routes[j].add(i) #mapping a stop to a route
+        
+        visited_routes = set()
+        visited_stops = set([source])
+
+        q = deque()
+        for i in stop_to_routes[source]:
+            q.append((i, 1))
+            visited_routes.add(i)
+
+        while q:
+            route, buses = q.popleft()
+            if target in routes[route]:
+                return buses
+            
+            for stop in routes[route]:
+                if stop not in visited_stops:
+                    for i in stop_to_routes[stop]:
+                        if i not in visited_routes:
+                            q.append((i, buses + 1))
+                            visited_stops.add(stop)
+                            visited_routes.add(route)
+        return -1
+
