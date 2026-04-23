7f0aa992476ee0d4d5f84b3dde435c1bd369c7d1
Spyridon Giakoumatos <105256569+millerdefoe@users.noreply.github.com>
2026-04-23 14:50:19 +0800

    Add: sql-immediate-food-delivery-i - submission-0

---
+++ SQL/sql-immediate-food-delivery-i/submission-0.sql
@@ -0,0 +1,6 @@
+-- Write your query below
+SELECT ROUND(
+    COUNT(CASE WHEN order_date = customer_pref_delivery_date THEN 1 END) * 100.0 / COUNT(*),
+    2
+) AS immediate_percentage
+FROM delivery;
