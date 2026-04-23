d21799230d57fffcdc8eaea5bb22d9fe8c347c1c
Spyridon Giakoumatos <105256569+millerdefoe@users.noreply.github.com>
2026-04-23 14:19:10 +0800

    Add: sql-warehouse-manager - submission-0

---
+++ SQL/sql-warehouse-manager/submission-0.sql
@@ -0,0 +1,6 @@
+-- Write your query below
+
+SELECT w.name as warehouse_name, sum(p.width * p.length * p.height * w.units) as volume
+FROM warehouse w
+LEFT JOIN products p ON w.product_id = p.product_id
+GROUP BY w.name;
