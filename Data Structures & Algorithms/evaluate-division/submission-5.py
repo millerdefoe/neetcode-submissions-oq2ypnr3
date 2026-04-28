565a32d10dab6efd2a3446f6961b822c1f81a64c
Spyridon Giakoumatos <105256569+millerdefoe@users.noreply.github.com>
2026-04-28 19:31:17 +0800

    Add: evaluate-division - submission-5

---
+++ Data Structures & Algorithms/evaluate-division/submission-5.py
@@ -0,0 +1,39 @@
+class Solution:
+    def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
+        adjMap = defaultdict(list)
+        eqn_to_val = {}
+        for eqn, val in zip(equations,values):
+            eqn_to_val[tuple(eqn)] = val
+            eqn_to_val[tuple(eqn[::-1])] = 1 / val
+
+        for equation in equations:
+            adjMap[equation[0]].append(equation[1])
+            adjMap[equation[1]].append(equation[0])
+        res = []
+        for query in queries:
+            # bfs to find path 
+            if query[1] not in adjMap or query[0] not in adjMap:
+                res.append(-1)
+            else:
+                #bfs to find the route
+                q = deque()
+                q.append((query[0],1))
+                found = 0
+                visited = {query[0]}
+                while q:
+                    letter, product = q.popleft()
+
+                    if letter == query[1]:
+                        res.append(product)
+                        found = 1
+                        break
+                    
+                    for nei in adjMap[letter]:
+                        if nei not in visited:
+                            visited.add(nei)
+                            q.append((nei, product * eqn_to_val[tuple((letter,nei))]))
+                if not found:
+                    res.append(-1)
+        
+        return res
+            
