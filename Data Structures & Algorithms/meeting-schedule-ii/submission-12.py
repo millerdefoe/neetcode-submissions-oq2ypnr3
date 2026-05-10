6f74bced6a4096b2fa7f34ec03192d364a1556d0
Spyridon Giakoumatos <105256569+millerdefoe@users.noreply.github.com>
2026-05-10 17:40:32 +0800

    Add: meeting-schedule-ii - submission-12

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-12.py
@@ -0,0 +1,25 @@
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+class Solution:
+    def minMeetingRooms(self, intervals: List[Interval]) -> int:
+        start = sorted([interval.start for interval in intervals])
+        end = sorted([interval.end for interval in intervals])
+
+        res, count = 0, 0
+        s, e = 0, 0
+        while s < len(intervals):
+            if start[s] < end[e]:
+                count += 1
+                s += 1
+            else:
+                count -= 1
+                e += 1
+            res = max(count, res)
+
+        return res
